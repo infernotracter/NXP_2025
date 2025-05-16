7664d09b8bc8347f24f126421afdf748cb31742b
infernotracter <zzc18007239316@163.com>
2025-05-16 19:01:04 +0800

    新车模数据

--- SRC_主分支_main_master/user_main.py
+++ SRC_主分支_main_master/user_main.py
@@ -247,7 +247,6 @@ key_data = key.get()
 imu_data = imu.get()
 imu_data_filtered = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0, 0, 0.0, 0.0]
 
-
 def clearall():
     key.clear(1)
     key.clear(2)
@@ -257,11 +256,11 @@ while True:
     
     motor_l.duty(my_limit(gyro_pid_out - dir_in_out, -3000, 3000))
     motor_r.duty(my_limit(gyro_pid_out + dir_in_out, -3000, 3000))
-    #ccd_temp_data = ccd.get(0)
-    #ccd_mid_point = ccd_near.get_mid_point(tmpdata = ccd_temp_data, value =31, reasonrange = 30, follow = 0, searchgap = 0)
-    
-    error1=64
-    error2=0
+    ccd_temp_data = ccd.get(0)
+    ccd_mid_point = ccd_near.get_mid_point(tmpdata = ccd_temp_data, value =31, reasonrange = 30, follow = 0, searchgap = 0)
+    ccd_mid_point_far=ccd_far.get_mid_point(tmpdata = ccd_temp_data, value =31, reasonrange = 30, follow = 0, searchgap = 0)
+    error1=ccd_mid_point-64
+    error2=ccd_mid_point_far-64
     #print("ccd_mid_point:", ccd_mid_point)
     # 拨码开关关中断
     if end_switch.value() == 1:
@@ -332,18 +331,25 @@ while True:
         distance.update(tmpdata = (encl_data + encr_data) / 2, delta_t = 0.4)
         # 定期进行数据解析
         #dir_out_out = dir_out.calculate(0, error1 + error2)
-#         data_flag = wireless.data_analysis()
-#         for i in range(0, 8):
+        data_flag = wireless.data_analysis()
+        for i in range(0, 8):
 #             # 判断哪个通道有数据更新
-#             if (data_flag[i]):
+            if (data_flag[i]):
 #                 # 数据更新到缓冲
-#                 data_wave[i] = wireless.get_data(i)
-#                 # 将更新的通道数据输出到 Thonny 的控制台
-#                 print("Data[{:<6}] updata : {:<.3f}.\r\n".format(
-#                     i, data_wave[i]))
-#                 dir_in.kp=data_wave[0]
-#                 dir_in.ki=data_wave[1]
+                data_wave[i] = wireless.get_data(i)
+                # 将更新的通道数据输出到 Thonny 的控制台
+                print("Data[{:<6}] updata : {:<.3f}.\r\n".format(
+                    i, data_wave[i]))
+                gyro_pid.kp=data_wave[0]
+                gyro_pid.ki=data_wave[1]
+                gyro_pid.kd=data_wave[2]
+                angle_pid.kp=data_wave[3]
+                angle_pid.ki=data_wave[4]
+                angle_pid.kd=data_wave[5]
+                speed_pid.kp=data_wave[6]
+                speed_pid.kd=data_wave[7]
 #         # 将数据发送到示波器
-#         wireless.send_oscilloscope(dir_in.kp,dir_in.ki)
+        wireless.send_oscilloscope(gyro_pid.kp,gyro_pid.ki,gyro_pid.kd,angle_pid.kp,angle_pid.ki,angle_pid.kd,speed_pid.kp,speed_pid.kd)
         ticker_flag_8ms = False
+
 
