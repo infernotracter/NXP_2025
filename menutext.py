f72962eb7793f642912bf9a0457a35ac239042f6
llllc114514 <2102441200@qq.com>
2025-07-14 11:19:36 +0800

    修bug

--- menutext.py
+++ menutext.py
@@ -27,46 +27,46 @@ class MenuText:
             self.show_speed_menu()
         if self.ring_menu:
             self.show_ring_menu()
-    def point_move(self, hight, low):
+    def point_move(self, hight, low,step):
         if self.key[1]:
             lcd.clear(0x0000)
-            self.point += self.string_size
+            self.point += step
             key.clear(2)
-            if self.point == hight + self.string_size:
+            if self.point == hight + step:
                 self.point = low
         if self.key[0]:
             lcd.clear(0x0000)
-            self.point -= self.string_size
+            self.point -= step
             key.clear(1)
-            if self.point == low - self.string_size:
+            if self.point == low - step:
                 self.point = hight
     def show_main_menu(self):
-        lcd.str16(16, 30, "ccd", 0xFFFF)
-        lcd.str16(16, 46, "turn", 0xFFFF)
-        lcd.str16(16, 62, "speed", 0xFFFF)
-        lcd.str16(16, 78, "ring", 0xFFFF)
-        lcd.str16(0, self.point, ">", 0xF800)
-        self.point_move(78, 30)
+        lcd.str24(48, 30, "ccd", 0xFFFF)
+        lcd.str24(48, 54, "turn", 0xFFFF)
+        lcd.str24(48, 78, "speed", 0xFFFF)
+        lcd.str24(48, 102, "ring", 0xFFFF)
+        lcd.str24(0, self.point, "8==D", 0xF800)
+        self.point_move(102, 30 , 24)
         if self.point == 30 and self.key[2]:
             key.clear(3)
             self.main_menu = False
             self.ccd_menu = True
-            self.point = 30
+            self.point = 170
             lcd.clear(0x0000)
         
-        if self.point == 46 and self.key[2]:
+        if self.point == 54 and self.key[2]:
             lcd.clear(0x0000)
             self.turn_menu = True
             self.main_menu = False
             self.point = 30
             key.clear(3)
-        if self.point == 62 and self.key[2]:
+        if self.point == 78 and self.key[2]:
             lcd.clear(0x0000)
             self.speed_meun = True
             self.main_menu = False
             self.point = 30
             key.clear(3)
-        if self.point == 78 and self.key[2]:
+        if self.point == 102 and self.key[2]:
             lcd.clear(0x0000)
             self.ring_menu = True
             self.main_menu = False
@@ -76,11 +76,11 @@ class MenuText:
     def show_ccd_menu(self):
         lcd.wave(0,  0, 128, 64, ccd_near.data, max = 255)
         lcd.wave(0,  64, 128, 64, ccd_far.data, max = 255)
-        lcd.str16(16 , 144, "value={}".format(self.value), 0xFFFF)
-        lcd.str16(16, 160,"return" , 0xFFFF)
+        lcd.str16(16 , 170, "value={}".format(self.value), 0xFFFF)
+        lcd.str16(16, 186,"return" , 0xFFFF)
         lcd.str16(0, self.point, ">", 0xF800)
-        self.point_move(160, 144)
-        if self.point == 144:
+        self.point_move(186, 170,16)
+        if self.point == 170:
             if self.key[2]:
                 key.clear(3)
                 self.value += 1
@@ -89,17 +89,17 @@ class MenuText:
                 key.clear(4)
                 self.value -= 1
                 lcd.clear(0x0000)
-        if self.point == 160 and self.key[2]:
+        if self.point == 186 and self.key[2]:
             key.clear(3)
             self.ccd_menu = False
             self.main_menu = True
             self.point = 30
             lcd.clear(0x0000)
     def show_turn_menu(self):
-        lcd.str16(16, 30, "turn out kp={}".format(-70.73+self.turn_out_kp_move), 0xFFFF)
+        lcd.str16(16, 30, "turn out kp={:.3f}".format(-70.73+self.turn_out_kp_move), 0xFFFF)
         lcd.str16(16, 46, "return", 0xFFFF)
         lcd.str16(0, self.point, ">", 0xF800)
-        self.point_move(46, 30)
+        self.point_move(46, 30,16)
         if self.point == 30:
             if self.key[2]:
                 key.clear(3)
@@ -118,9 +118,11 @@ class MenuText:
     
     def show_speed_menu(self):
         lcd.str16(16, 30, "tmp_speed={}".format(speed_controller.tmp_speed), 0xFFFF)
-        lcd.str16(16, 46, "return", 0xFFFF)
+        lcd.str16(16, 46, "start",0xFFFF)
+        lcd.str16(16, 62, "return", 0xFFFF)
+        lcd.str24(16, 100,"start_flag={}".format(speed_controller.start_flag),0xFFFF)
         lcd.str16(0, self.point, ">", 0xF800)
-        self.point_move(46, 30)
+        self.point_move(62, 30,16)
         if self.point == 30:
             if self.key[2]:
                 key.clear(3)
@@ -132,6 +134,14 @@ class MenuText:
                 lcd.clear(0x0000)
         if self.point == 46 and self.key[2]:
             key.clear(3)
+            if speed_controller.start_flag == 0:
+                speed_controller.start_flag=1
+            else:
+                speed_controller.start_flag=0
+            lcd.clear(0x0000)
+        
+        if self.point == 62 and self.key[2]:
+            key.clear(3)
             self.speed_meun = False
             self.main_menu = True
             self.point = 30
@@ -140,7 +150,7 @@ class MenuText:
     def show_ring_menu(self):
         lcd.str16(16, 30,"DISTANCE_ring3_data={}".format(elementdetector.DISTANCE_ring3_data), 0xFFFF)
         lcd.str16(16, 46, "DISTANCE_ring_in_data={}".format(elementdetector.DISTANCE_ring_in_data), 0xFFFF)
-        lcd.str16(16, 62, "DISTANCE_ring_outcoming_data={}".format(elementdetector.DISTANCE_ring_outcoming_data), 0xFFFF)
+        lcd.str16(16, 62, "DISTANCE_outcoming_data={}".format(elementdetector.DISTANCE_ring_outcoming_data), 0xFFFF)
         lcd.str16(16, 78, "DISTANCE_ring_out_data={}".format(elementdetector.DISTANCE_ring_out_data), 0xFFFF)
         lcd.str16(16, 94, "ERROR_l_out_value={}".format(elementdetector.ERROR_l_out_value), 0xFFFF)
         lcd.str16(16, 110, "return", 0xFFFF)
@@ -148,7 +158,7 @@ class MenuText:
         lcd.str24(0, 134, "gyro={:.2f}".format(element_gyro.data), 0xFFFF)
         lcd.str24(0, 158, "dis={:.2f}".format(element_distance.data), 0xFFFF)
         lcd.str24(0, 182, "state={}".format(elementdetector.state), 0xFFFF)
-        self.point_move(110, 30)
+        self.point_move(110, 30,16)
         if self.point == 30:
             if self.key[2]:
                 key.clear(3)
